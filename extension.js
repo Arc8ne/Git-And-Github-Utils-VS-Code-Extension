428145ec5ab26db08a978ee3783e3adc5bbf5834
Arc8ne <naveed.fazlan@gmail.com>
2023-10-22 01:55:27 +0800

    Backup

--- extension.js
+++ extension.js
@@ -12,16 +12,16 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "git-and-github-utilities" is now active!');
+	console.log('The extension "git-and-github-utilities" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('git-and-github-utilities.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('git-and-github-utilities.versionCheck', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Git and Github Utilities!');
+		vscode.window.showInformationMessage('Current version: 0.1.0');
 	});
 
 	context.subscriptions.push(disposable);
